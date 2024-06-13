a152a54f217dd09c94459b426f176d0bc75d5de7
Unventured <amirbombata@gmail.com>
2024-06-13 16:28:33 -0700

    getting started on javascript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+document.write("Hello, World!");
+var a = "\"This is a string\"" + "is what I was told to write for the assigment.";
+document.write(a);
+window.alert(a);
+var Family = "The Smiths", Dad = "Joe", Mom = "Susan", Son = " Kevin";
+document.write(Son);
+document.write(5+5);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JavaScript-Projects
+This repository is where I'll put all my JavaScript assignments from The Tech Academy.
