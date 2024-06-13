a34f736a7cd7eb8a3cadd5911ea2af75cd6461bb
Unventured <amirbombata@gmail.com>
2024-06-13 16:50:59 -0700

    Wrote a function

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,6 +2,6 @@ document.write("Hello, World!");
 var a = "\"This is a string\"" + "is what I was told to write for the assigment.";
 document.write(a);
 window.alert(a);
-var Family = "The Smiths", Dad = "Joe", Mom = "Susan", Son = " Kevin";
-document.write(Son);
-document.write(5+5);
+var Family = "The Smiths", Dad = "Joe", Mom = "Susan", Son = " Kevin"; //Assigning multiple variables in one line.
+document.write(Son); //Displaying a variable
+document.write(5+5); //Displaying an expression
