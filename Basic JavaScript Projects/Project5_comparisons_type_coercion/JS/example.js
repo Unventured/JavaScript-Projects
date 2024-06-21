f74b94d51450d19ecbfe1f9be16b0fbda709355f
Unventured <amirbombata@gmail.com>
2024-06-21 15:29:42 -0700

    Utilized typeof, combined strings and numbers, used logical operators

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/example.js
@@ -0,0 +1,5 @@
+function my_Function(){
+    document.getElementById("Test1").innerHTML = 0/0;
+    document.getElementById("Test2").innerHTML = isNaN("Hello");
+    document.getElementById("Test3").innerHTML = isNaN(14);
+}
